011f0ea39ba850d38385e25804aec7939039d3b6
Rinsi Gupta <rinsigupta7982@gmail.com>
2024-04-25 16:31:51 +0530

    Commited

---
+++ script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('form');
+const list = document.querySelector('#list');
+const inp = document.querySelector('#inp');
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const inpText = inp.value;
+    inp.value = '';
+    getMovie(inpText);
+});
+
+function getMovie(searchText) {
+    const URL = `https://api.tvmaze.com/search/shows?q=${searchText}`
+    
+    list.innerText = '';
+    
+    axios.get(URL)
+    .then((res)=>{
+        const data = res.data;
+        data.forEach(obj => {
+            // const image = obj.show.image.medium;
+            const image = document.createElement('img');
+            image.setAttribute('src', obj.show.image.medium);
+            image.style.argin = '10px';
+            list.append(image);
+        });
+    })
+    
+    .catch(err => console.log(err));
+}
